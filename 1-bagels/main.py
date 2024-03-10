f9fc058f803fe9aacc487d141506453eafadec7e
randyarbolaez <randyarbolaez@Outlook.com>
2024-03-10 14:51:03 -0400

    while loop for guesses

--- 1-bagels/main.py
+++ 1-bagels/main.py
@@ -11,8 +11,14 @@ def main():
         Bagels            No digit is correct.
     I have thought up a number.''')
     randomInt = random.randrange(100,999)
+    guessInt = 1
     print("You have 10 guesses to get it.")
+    while guessInt != 11:
+        print(f'Guess #{guessInt}: \n')
+        guessInt += 1
 
+    print("Do you want to play again? (yes or no)")
+    playAgain = input();
 
 if __name__ == "__main__":
     main()
