09273fe83029262065203d42af2acd802ec0742c
randyarbolaez <randyarbolaez@Outlook.com>
2024-03-13 15:55:06 -0400

    user has to enter intput when asked for a guess

--- 1-bagels/main.py
+++ 1-bagels/main.py
@@ -12,13 +12,32 @@ def main():
     I have thought up a number.''')
     randomInt = random.randrange(100,999)
     guessInt = 1
+    intGuessed = None
     print("You have 10 guesses to get it.")
     while guessInt != 11:
-        print(f'Guess #{guessInt}: \n')
+        print(f'Guess #{guessInt}:')
+        intGuessed = input()
+        isTheIntGuessedAnInt(intGuessed)
         guessInt += 1
 
     print("Do you want to play again? (yes or no)")
     playAgain = input();
+
+def userInputCorrectType(userInput):
+    # print(not userInput.isnumeric())
+    print("")
+    while not userInput.isnumeric():
+        print("hello. plz enter correct type : ")
+        userInput = input()
+    print("thank you.")
+    return userInput
+
+def isTheIntGuessedAnInt(intGuessedParam):
+    # intGuessedParam = int(intGuessedParam)
+    if intGuessedParam.isnumeric():
+        return intGuessedParam
+    else:
+        userInputCorrectType(intGuessedParam)
 
 if __name__ == "__main__":
     main()
